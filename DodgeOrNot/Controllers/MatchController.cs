3958faaec37d3d3f87b4c081aae874dfc10a2849
Eric Pan <erzpan@gmail.com>
2016-05-04 05:20:36 -0500

    working AJAX for summoners and matches

---
+++ DodgeOrNot/Controllers/MatchController.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+using DodgeOrNot.Models;
+
+using Newtonsoft.Json;
+
+namespace DodgeOrNot.Controllers
+{
+    public class MatchController : Controller
+    {
+        // GET: Match
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        // GET: Match/Test
+        public string Test()
+        {
+            return "this is THE match C0ntr0ll3r!!11!!";
+        }
+
+        public string GetHistory(string region, long summonerID, string matchType, int beginIndex, int endIndex)
+        {
+            Match[] matches = Match.GetMatchesFor(region, summonerID, matchType, beginIndex, endIndex);
+            return JsonConvert.SerializeObject(matches);
+        }
+
+        public string GetResults(string region, long summonerID, string matchType, int beginIndex, int endIndex)
+        {
+            MatchResult[] matchResults = MatchResult.GetMatchResultsFor(region, summonerID, matchType, beginIndex, endIndex);
+            return JsonConvert.SerializeObject(matchResults);
+        }
+    }
+}
---
+++ DodgeOrNot/Models/Region.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace DodgeOrNot.Models
+{
+    public static class Region
+    {
+        public static readonly string NA = "na";
+        public static readonly string EUW = "euw";
+        public static readonly string LAN = "lan";
+    }
+}
